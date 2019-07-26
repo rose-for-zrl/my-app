b5b1a0489efc8010cc4824dd4513ec0b9277f99a
rose-for-zrl <zrl1149591130@gmail.com>
2019-07-26 18:53:49 +0800

    1

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import * as serviceWorker from './serviceWorker';
 class Square extends React.Component {
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button className="square" onClick={()=>alert("click")}>
+                {this.props.value}
             </button>
         );
     }
@@ -16,7 +16,7 @@ class Square extends React.Component {
 
 class Board extends React.Component{
     renderSquare(i){
-        return <Square />;
+        return <Square value={i}/>;
     }
 
     render() {
@@ -25,15 +25,42 @@ class Board extends React.Component{
             <div>
                 <div className="status">{status}</div>
                 <div className="board-row">
-
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
                 </div>
             </div>
         );
     }
 }
 
+class Game extends React.Component{
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/**/}</div>
+                    <div>{/**/}</div>
+                </div>
+            </div>
+        );
+    }
+}
 ////////////////////////////////////////////////////////////////////////////
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
