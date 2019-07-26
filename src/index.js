f69a926236200ad644c1cf7def616fd5a31e4b11
rose-for-zrl <zrl1149591130@gmail.com>
2019-07-26 18:36:55 +0800

    temp

--- src/index.js
+++ src/index.js
@@ -4,6 +4,35 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {/* TODO */}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component{
+    renderSquare(i){
+        return <Square />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+
+                </div>
+            </div>
+        );
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
